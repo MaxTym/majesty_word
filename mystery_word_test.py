5a6d0cb5b9228e3fa3f36289d7d77ca1b2a67de4
Maksym Tymtsiv <mtymtsiv@gmail.com>
2016-11-01 09:02:10 -0400

    Add files via upload

--- mystery_word_test.py
+++ mystery_word_test.py
@@ -1,8 +1,8 @@
 from mystery_word import lettet_in_secret_word
 
 
-def guess_check_true():
+def test_guess_check_true():
     assert lettet_in_secret_word('a', 'apple') == True
 
-def guess_check_false():
-    assert lettet_in_secret_word('b', 'apple') == True
+def test_guess_check_false():
+    assert lettet_in_secret_word('b', 'apple') != True

c56f537710adb55b81fce2a786e22c44e532907d
Maksym Tymtsiv <mtymtsiv@gmail.com>
2016-12-05 17:26:37 -0500

    Update mystery_word.py

--- mystery_word.py
+++ mystery_word.py
@@ -1,8 +1,8 @@
 import random
 import os
 
-def choose_difficulty_level():
-    global random_word
+
+def get_secret_word():
     while True:
         dif_level = input("Choose a level of difficulty: \n ['E'asy, 'N'ormal, 'Hard']").lower()
         if len(dif_level) >1:
@@ -20,6 +20,7 @@ def choose_difficulty_level():
             break
     return random_word
 
+
 def give_me_easy_words():
     comb_list = []
     easy_words = []
@@ -31,6 +32,7 @@ def give_me_easy_words():
             if len(str(word)) > 3 and len(str(word)) < 6:
                 easy_words.append(word)
     return easy_words
+
 
 def give_me_norm_words():
     comb_list = []
@@ -44,6 +46,7 @@ def give_me_norm_words():
                 norm_words.append(word)
     return norm_words
 
+
 def give_me_hard_words():
     comb_list = []
     hard_words = []
@@ -56,31 +59,29 @@ def give_me_hard_words():
                 hard_words.append(word)
     return hard_words
 
-def input_check():
+
+def input_check(secret_word):
     good_guesses = []
     bad_guesses = []
-    disp = '_'*len(random_word)
+    disp = '_'*len(secret_word)
     guess_count = 8
     while '_' in disp and guess_count > 0:
-        user_guess()
+        guess = user_guess()
         if len(guess) > 1:
             print("One letter at the time")
         elif not guess.isalpha():
             print("Letters only")
         elif guess == '^q':
             break
-        elif '_' not in disp:
-            print("You won!!!")
-            break
         elif guess in good_guesses or guess in bad_guesses:
             print("You've already tried it")
         else:
-            if lettet_in_secret_word(guess, random_word):
+            if lettet_in_secret_word(guess, secret_word):
                 print("You've got it! Attempts left: {}".format(guess_count))
                 good_guesses += guess
                 index = 0
-                while len(random_word) > 0:
-                    index = random_word.find(guess, index)
+                while len(secret_word) > 0:
+                    index = secret_word.find(guess, index)
                     if index == -1:
                         break
                     disp = disp[:index] + guess + disp[(index + 1):]
@@ -92,38 +93,51 @@ def input_check():
             print(disp)
     else:
         if guess_count == 0:
-            print("You are out of guesses!")
-            print("The word was: {}".format(random_word))
-            if play_again():
-                main()
-            else:
-                os.system('clear')
+            lose_condition(secret_word)
         else:
-            print("You won!!!")
-            if play_again():
-                main()
-            else:
-                os.system('clear')
+            win_condition()
+
+
+def lose_condition(secret_word):
+    print("You are out of guesses!")
+    print("The word was: {}".format(secret_word))
+    if play_again():
+        main()
+    else:
+        os.system('clear')
+
+
+def win_condition():
+    print("You won!!!")
+    if play_again():
+        main()
+    else:
+        os.system('clear')
+
 
 def user_guess():
     global guess
     guess = input("Enter a letter: ").lower()
     return guess
 
+
 def lettet_in_secret_word(guess, random_word):
     if guess in random_word:
         return True
+
 
 def play_again():
     play_again = input("Do you want to play again?: \n ['Y'/'n']").lower()
     if play_again == '' or play_again == 'y':
         return True
 
+
 def main():
     os.system('clear')
-    choose_difficulty_level()
-    print('_'*len(random_word))
-    input_check()
+    secret_word = get_secret_word()
+    print('_'*len(secret_word))
+    input_check(secret_word)
+
 
 if __name__ == '__main__':
     main()

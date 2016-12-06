138ce96957afa27535730428720bba6b844ed8ca
Maksym Tymtsiv <mtymtsiv@gmail.com>
2016-12-05 21:22:55 -0500

    Update mystery_word_test.py

--- mystery_word_test.py
+++ mystery_word_test.py
@@ -1,8 +1,17 @@
-from mystery_word import lettet_in_secret_word
+from mystery_word import valid_guess, lettet_in_secret_word
 
 
 def test_guess_check_true():
     assert lettet_in_secret_word('a', 'apple') == True
 
+
 def test_guess_check_false():
     assert lettet_in_secret_word('b', 'apple') != True
+
+
+def test_valid_input_false():
+    assert valid_guess('1') != True
+
+
+def test_valid_input_true():
+    assert valid_guess('d') == True
